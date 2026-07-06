ef9400dca9f419942447eec607afd75e22a143a6
SunainaPawar07 <pawarsunaina4@gmail.com>
2026-07-06 15:45:25 +0530

    Add files via upload

---
+++ 02_Introduction_to_SQL/Retrieving Data Using the SQL SELECT Statement/Retrieving_SQL_assessment.sql
@@ -0,0 +1,26 @@
+/* Question 01.
+Determine the Structure of DEPT Table and its Contents.
+
+Query:-
+*/
+DESCRIBE DEPARTMENT;
+
+/* Question 02.
+Determine the Structure of EMP Table and its Contents.
+
+Query:-
+*/
+DESCRIBE EMPLOYEE;
+
+/*Question 03.
+Display the ENAME and DEPTNO from EMP table whose EMPNO is 7788.
+
+Query:-
+*/
+SELECT FIRST_NAME, DEPARTMENT_ID
+FROM EMPLOYEE
+WHERE EMPLOYEE_ID = 7788;
+
+
+
+
